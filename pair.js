05e8943d08e5b620e5e7ed3dcf21e4de4f454761
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-20 20:36:52 +0300

    fixed pair code ✅

--- pair.js
+++ pair.js
@@ -19,13 +19,6 @@ router.get('/', async (req, res) => {
             saveCreds
         } = await useMultiFileAuthState('./temp/' + id);
         try {
-var items = ["Safari"];
-function selectRandomItem(array) {
-  var randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-var randomItem = selectRandomItem(items);
-            
             let sock = makeWASocket({
                 auth: {
                     creds: state.creds,
@@ -35,7 +28,7 @@ var randomItem = selectRandomItem(items);
                 generateHighQualityLinkPreview: true,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
                 syncFullHistory: false,
-                browser: Browsers.macOS(randomItem)
+                browser: Browsers.macOS('Chrome') // Changed from randomItem (Safari) to Chrome
             });
             if (!sock.authState.creds.registered) {
                 await delay(1500);
@@ -48,7 +41,7 @@ var randomItem = selectRandomItem(items);
             sock.ev.on('creds.update', saveCreds);
             sock.ev.on("connection.update", async (s) => {
 
-    const {
+                const {
                     connection,
                     lastDisconnect
                 } = s;
@@ -69,9 +62,6 @@ var randomItem = selectRandomItem(items);
                     }
                     const randomText = generateRandomText();
                     try {
-
-
-                        
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
@@ -99,40 +89,40 @@ var randomItem = selectRandomItem(items);
 
 > *© Powered by peacemaker Cyber 🚀*`; 
                         await sock.sendMessage(sock.user.id, {
-text: desc,
-contextInfo: {
-externalAdReply: {
-title: "PEACEMAKER",
-thumbnailUrl: "https://files.catbox.moe/7pg2gp.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z",
-mediaType: 1,
-renderLargerThumbnail: true
-}  
-}
-},
-{quoted:code })
+                            text: desc,
+                            contextInfo: {
+                                externalAdReply: {
+                                    title: "PEACEMAKER",
+                                    thumbnailUrl: "https://files.catbox.moe/7pg2gp.jpg",
+                                    sourceUrl: "https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z",
+                                    mediaType: 1,
+                                    renderLargerThumbnail: true
+                                }  
+                            }
+                        },
+                        { quoted: code })
                     } catch (e) {
-                            let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `*Don't Share with anyone this code is used for deployment of PEACEMAKER*\n\n ◦ *Github:* https://github.com/Peacemaker-cyber/Peacemaker-md`;
-                            await sock.sendMessage(sock.user.id, {
-text: desc,
-contextInfo: {
-externalAdReply: {
-title: "PEACEMAKER",
-thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
-mediaType: 2,
-renderLargerThumbnail: true,
-showAdAttribution: true
-}  
-}
-},
-{quoted:ddd })
+                        let ddd = sock.sendMessage(sock.user.id, { text: e });
+                        let desc = `*Don't Share with anyone this code is used for deployment of PEACEMAKER*\n\n ◦ *Github:* https://github.com/Peacemaker-cyber/Peacemaker-md`;
+                        await sock.sendMessage(sock.user.id, {
+                            text: desc,
+                            contextInfo: {
+                                externalAdReply: {
+                                    title: "PEACEMAKER",
+                                    thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
+                                    sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
+                                    mediaType: 2,
+                                    renderLargerThumbnail: true,
+                                    showAdAttribution: true
+                                }  
+                            }
+                        },
+                        { quoted: ddd })
                     }
                     await delay(10);
                     await sock.ws.close();
                     await removeFile('./temp/' + id);
-                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
+                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ✅ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼c𝗲𝘀𝘀...`);
                     await delay(10);
                     process.exit();
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
