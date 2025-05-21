3c726f454ae0911c1ee575689e83eb7c51bf858b
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-21 12:43:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star PEACEMAKER-MD 
+Don't Forgot To Give Star.
 
  Server running on http://localhost:` + PORT)
 })
